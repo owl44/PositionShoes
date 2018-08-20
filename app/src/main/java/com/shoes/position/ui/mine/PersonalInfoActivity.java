e22e9d5b2dea3eae6e0603939a4695a90b72a409
zrj <1106977428@qq.com>
2018-08-20 17:14:11 +0800

    search select img back head portrait

--- app/src/main/java/com/shoes/position/ui/mine/PersonalInfoActivity.java
+++ app/src/main/java/com/shoes/position/ui/mine/PersonalInfoActivity.java
@@ -1,18 +1,32 @@
 package com.shoes.position.ui.mine;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.joooonho.SelectableRoundedImageView;
 import com.shoes.position.R;
 import com.shoes.position.base.BaseActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class PersonalInfoActivity extends BaseActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_personal_info);
-    }
+    @BindView(R.id.head_img)
+    SelectableRoundedImageView headImg;
+    @BindView(R.id.mine_head_img)
+    LinearLayout mineHeadImg;
+    @BindView(R.id.nike_name)
+    TextView nikeName;
+    @BindView(R.id.mine_nike_name)
+    LinearLayout mineNikeName;
+    @BindView(R.id.phone_number)
+    TextView phoneNumber;
+    @BindView(R.id.mine_phone_number)
+    LinearLayout minePhoneNumber;
 
     @Override
     public void setContentLayout() {
@@ -22,5 +36,17 @@ public class PersonalInfoActivity extends BaseActivity {
     @Override
     protected void setLogic() {
 
+    }
+
+    @OnClick({R.id.mine_head_img,R.id.mine_nike_name,R.id.mine_phone_number})
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.mine_head_img:
+                break;
+            case R.id.mine_nike_name:
+                break;
+            case R.id.mine_phone_number:
+                break;
+        }
     }
 }
